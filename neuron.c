75d26b5ebcfb6126dc630c981e6db417cd64d9e6
theshteves <stevenkneiser@gmail.com>
2024-01-13 03:01:42 -0500

    Build 0.0.8: Ghostbusters!Neuron now blinks relative to the strongest Wi-Fi RSSI around

--- neuron.c
+++ neuron.c
@@ -1,60 +1,63 @@
 #include <math.h>
 #include <stdio.h>
+//TODO: optimize printf's into write's?
 
-#include "hardware/adc.h"
 #include "pico/cyw43_arch.h"
 #include "pico/multicore.h"
 #include "pico/stdlib.h"
 
 
-/*
- * Empirical fit to an exponential curve
- * (e.g. {80, 2000} {89, 1000} {95, 400} {100, 200})
- * for a visually-intuitive temperature
- */
-int delay_ms_adjusted_by_temperature_c(float temperature_c) {
-  double exponent = pow(2.7, (-0.096 * temperature_c));
-  long int delay_ms = 4096000 * exponent;
-  //TODO: math.clamp floor & ceiling?
-  return delay_ms;
-}
-
-
-/*
- * Blink LED at a frequency relative to on-board temperature
- */
-void blink_temperature() {
-  adc_init();
-  adc_gpio_init(26);
-  adc_select_input(4); // on-board temperature sensor
-
-  while (true) {
-    uint16_t result = adc_read();
-    float voltage = result * 3.3f / (1 << 12);
-    float temperature_c = 27 - (voltage - 0.706f) / 0.001721f;
-
-    int delay_ms = delay_ms_adjusted_by_temperature_c(temperature_c);
-    printf("temp(C): %.2f  =>  delay(ms): %dms\n", temperature_c, delay_ms);
-
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); // on-board LED
-    sleep_ms(delay_ms);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-    sleep_ms(delay_ms);
-  }
-}
+static int16_t BEST_WIFI_RSSI = -110; // No signal
+static int16_t NEURON_ACTIVATION_THRESHOLD = -30;
 
 
 /*
  * "Inspired by" pico-examples/pico_w/wifi/wifi_scan/picow_wifi_scan.c
  */
 static int scan_result(void *env, const cyw43_ev_scan_result_t *result) {
-    if (result) {
-        printf("ssid: %-32s rssi: %4d chan: %3d mac: %02x:%02x:%02x:%02x:%02x:%02x sec: %u\n",
-            result->ssid, result->rssi, result->channel,
-            result->bssid[0], result->bssid[1], result->bssid[2], result->bssid[3], result->bssid[4], result->bssid[5],
-            result->auth_mode);
+    if (result && result->rssi > BEST_WIFI_RSSI) {
+        BEST_WIFI_RSSI = result->rssi;
+
+        printf("\n==> %4d  %-32s chan: %3d sec: %u mac: %02x:%02x:%02x:%02x:%02x:%02x",
+            result->rssi, result->ssid, result->channel, result->auth_mode,
+            result->bssid[0], result->bssid[1], result->bssid[2], // OUI
+            result->bssid[3], result->bssid[4], result->bssid[5]);
     }
     return 0;
+}
+
+
+/*
+ * Empirical fit to a line
+ * (e.g. {-30, 50}, {-35, 100}, {-40, 150}, ..., {-75, 500})
+ * for a visually-intuitive best Wi-Fi RSSI
+ */
+int delay_ms_adjusted_by_best_wifi_rssi(int16_t best_wifi_rssi) {
+  //TODO: math.clamp floor & ceiling?
+  //return -10 * (int)best_wifi_rssi - 250;
+  //return MAX(15, -15 * (int)best_wifi_rssi - 350);
+  //return MAX(15, -30 * (int)best_wifi_rssi - 780);
+  return MAX(15, -10 * (int)best_wifi_rssi - 150);
+  // sub-15ms not noticeable to many humans
+}
+
+
+/*
+ * Blink LED at a frequency relative to best Wi-Fi RSSI
+ */
+void blink_best_wifi_rssi() {
+  while (true) {
+    int delay_ms = delay_ms_adjusted_by_best_wifi_rssi(BEST_WIFI_RSSI);
+    //printf("\n* blink(%d) = %dms", BEST_WIFI_RSSI, delay_ms);
+
+    printf("+");
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); // on-board LED
+    sleep_ms(delay_ms);
+
+    printf("-");
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(delay_ms);
+  }
 }
 
 
@@ -65,31 +68,44 @@ int main(int argv, const char *argc[]) {
     return 1;
   }
 
-  multicore_launch_core1(blink_temperature);
+  multicore_launch_core1(blink_best_wifi_rssi);
   cyw43_arch_enable_sta_mode();
 
   absolute_time_t scan_time = nil_time;
   bool scan_in_progress = false;
   while(true) {
       if (absolute_time_diff_us(get_absolute_time(), scan_time) < 0) {
+
+          // Start scanning if not
           if (!scan_in_progress) {
               cyw43_wifi_scan_options_t scan_options = {0};
+
+              // No? start a new one
               int err = cyw43_wifi_scan(&cyw43_state, &scan_options, NULL, scan_result);
               if (err == 0) {
-                  printf("\nPerforming wifi scan\n");
+                  printf("\n___ ___ ___ ___ ___ ___ ___ ___\n");
                   scan_in_progress = true;
+
               } else {
-                  printf("Failed to start scan: %d\n", err);
-                  scan_time = make_timeout_time_ms(10000); // wait 10s and scan again
+                  printf("\n\n[ERROR: Failed to start scan: %d]\n\n", err);
+                  scan_time = make_timeout_time_ms(800); //800?
               }
+
+          // Did scan just stop?
           } else if (!cyw43_wifi_scan_active(&cyw43_state)) {
-              scan_time = make_timeout_time_ms(10000); // wait 10s and scan again
+              scan_time = make_timeout_time_ms(500); // break between scans
               scan_in_progress = false; 
           }
       }
       
-      printf("` tick");
-      sleep_ms(1000);
+      printf("`");
+      sleep_ms(100);
+      BEST_WIFI_RSSI -= 1; // Forget best rssi reading over time (decay 10 per second)
+
+      if (BEST_WIFI_RSSI > NEURON_ACTIVATION_THRESHOLD) {
+          printf("<!>");
+          //TODO: DO WHAT NEURONS ARE SUPPOSED TO DO!
+      }
   }
 
   cyw43_arch_deinit();
